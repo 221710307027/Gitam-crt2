1a1cef2c174be944aa340cbda28b4aeedfcbeb30
221710307027 <58929663+221710307027@users.noreply.github.com>
2019-12-19 11:53:28 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a=123456;
+    long int b=12346789;
+    printf("a=%d",a);
+    printf("\nb=%d",b);
+    printf("\n&a=%d",&a);
+
+    return 0;
+}
