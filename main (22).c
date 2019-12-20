9cb42ee045c92c6e3f2a80bdbb224b2729e2223f
221710307027 <58929663+221710307027@users.noreply.github.com>
2019-12-20 10:07:51 +0530

    Add files via upload

---
+++ main (22).c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    int a=10,b=20,c;
+    c=a>b?a:b;
+    printf("%d",c);
+    return 0;
+}
